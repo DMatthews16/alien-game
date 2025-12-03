9cdbbdd7d7c8eee0dfb8955c8b539aec7f89dffe
DMatthews16 <diamondmatthews2434@gmail.com>
2025-12-03 00:18:04 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,68 @@
+import pygame 
+
+class Settings:            #class to store all the settings for the game Alien Invasion
+
+
+    def __init__(self):
+        #Initialize the game's static settings
+                # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        
+
+             # Ship settings
+        self.ship_limit = 3
+
+             # Bullet settings
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 15
+
+             # Alien settings
+        self.fleet_drop_speed = 10
+
+             # How quickly the game speeds up
+        self.speedup_scale = 1.5
+
+        self.difficulty_level = 'medium'
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+             #Initialize settings that change throughout the game
+        if self.difficulty_level == 'easy':
+            self.ship_limit = 5
+            self.bullets_allowed = 15
+            self.ship_speed = 2
+            self.bullet_speed = 6
+            self.alien_speed = 2
+        elif self.difficulty_level == 'medium':
+            self.ship_limit = 3
+            self.bullets_allowed = 15
+            self.ship_speed = 4
+            self.bullet_speed = 6
+            self.alien_speed = 4
+        elif self.difficulty_level == 'difficult':
+            self.ship_limit = 2
+            self.bullets_allowed = 15
+            self.ship_speed = 6
+            self.bullet_speed = 6.0
+            self.alien_speed = 6
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        #Increase speed settings
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+    def set_difficulty(self, diff_setting):
+        if diff_setting == 'easy':
+            print('easy')
+        elif diff_setting == 'medium':
+            pass
+        elif diff_setting == 'difficult':
+            pass
